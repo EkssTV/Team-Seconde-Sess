835ed42332f4095906690f88315ca29b14d040f9
EkssTV <matthieu.decreme@gmail.com>
2025-10-29 14:45:00 +0100

    UI et loop

---
+++ game.py
@@ -0,0 +1,12 @@
+import pygame
+from ui.screen import Screen
+#Création de la loop du jeu
+class Game :
+    def __init__(self):
+        self.running = True
+        self.screen = Screen()
+
+
+    def run(self):
+        while self.running :
+            pass
