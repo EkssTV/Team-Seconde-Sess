29fd98b9c8ebda3a8fe351145cf6d3194ecef72a
Erreur_504 <stefantroch@gmail.com>
2025-10-29 19:17:06 +0100

    aa

--- game/quizz/quizz_ohmlette.py
+++ game/quizz/quizz_ohmlette.py
@@ -1,8 +1,9 @@
 from quizz import Quizz
 #utilisation de la classe QUIZZ pour créer les différentes questions
 question1 = Quizz("Quelle est la bonne réponse?",
-                  ["la réponse 4","la réponse 4","la réponse 4","la réponse 4"],3,"Ohmlette")
-
+                  ["la réponse 4","la réponse 4","la réponse 4","la réponse 4"],
+                  3,
+                  "Ohmlette")
 
 
 
---
+++ game/quizz/quizz_secretaire.py
@@ -0,0 +1,6 @@
+from quizz import Quizz
+
+question1 = Quizz("La question",
+                  ["La","Liste","des","reponses"],
+                  4,
+                  "secrétaire")
