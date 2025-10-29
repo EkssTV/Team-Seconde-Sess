643aedf15755e1add197963fabc0b43f2e4d3ada
Erreur_504 <stefantroch@gmail.com>
2025-10-29 14:30:30 +0100

    correstion selon les standards de St. Matthieu.

--- game/player/player.py
+++ game/player/player.py
@@ -1,14 +1,14 @@
 
 
-                    """Création de classe player"""
+#Création de classe player
 class Player():
-                    """param vie + nom (protégé)"""
+#param vie + nom (protégé)
     def __init__(self, health: int = 100, name: str ):
         self.__name = name
         self.health = health
-                    """soigner le joueur"""
-    def heal(self,amount:int):
+#soigner le joueur
+    def add_health(self,amount:int):
         self.health += amount
-                """" fct str pour impression""""
+#fct str pour impression
     def __str__(self):
         return f'blabbla'
