dc8eed2404125ac84bcd3963225a36c4c5e3b9fd
Erreur_504 <stefantroch@gmail.com>
2025-10-29 17:08:29 +0100

    création class quizz, ask(). test avec quizz_ohmlette.py

--- game/quizz/quizz_ohmlette.py
+++ game/quizz/quizz_ohmlette.py
@@ -1,7 +1,6 @@
 from quizz import Quizz
-
+#utilisation de la classe QUIZZ pour créer les différentes questions
 question1 = Quizz("Quelle est la bonne réponse?",
                   ["la réponse 4","la réponse 4","la réponse 4","la réponse 4"],4,"Ohmlette")
 
 
-print(question1.ask())
