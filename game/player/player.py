36a89aa52d4a4a56dbaa1e4ee7d656595bc50f43
GrainDeRiz <gregoryly45@gmail.com>
2025-10-29 16:08:06 +0100

    Update player

--- game/player/player.py
+++ game/player/player.py
@@ -1,14 +1,29 @@
 
 
 #Création de classe player
-class Player():
-#param vie + nom (protégé)
-    def __init__(self, health: int = 5, name: str ='Student'  ):
+class Player:
+#Définition des caractéristiques du player
+    def __init__(self, health: int = 5, name: str ='Student', start_area: str = "place de l'Ephec"):
         self.__name = name
         self.health = health
-#soigner le joueur
+        self.current_area = start_area
+    #Savoir si le personnage est vivant ou non
+        self.alive = True
+    #Sauvegarde du personnage
+        self.save_slot = "save_1"
+
+
+#Fonctions du player
+    #Ajouter de la vie au player
     def add_health(self,amount:int):
         self.health += amount
-#fct str pour impression
+    #Retirer de la vie au player
+    def rmv_health(self,amount:int):
+        self.health -= amount
+    #Mort du personnage
+    def death(self):
+        self.alive = False
+        print(f"Ta perdu chacal")
+    #fct str pour impression
     def __str__(self):
         return f'blabbla'
