70660842a6a18308a3184726d16157d3f154b54d
GrainDeRiz <gregoryly45@gmail.com>
2025-10-29 19:20:13 +0100

    JPP tout le jeu est deja fait

--- game/area/world.py
+++ game/area/world.py
@@ -1,41 +1,43 @@
 from area import Area
-
-PLA01 = Area("place de l'Ephec",
+map = {
+"PLA01" : Area("place de l'Ephec",
              "PLA01",
              ["Parking", "HAL01"],
               ["personnage1"],
              ["objects"],
              "La place de l'Ephec,devant le bâtiment",
-             "Y a pleins d'élèves la")
+             "Y a pleins d'élèves la"),
 
-HAL01 = Area("Hall d'entrée",
+"HAL01" : Area("Hall d'entrée",
              "HAL01",
              ["PLA01", "Ancienne aile RDC", "Nouvelle aile RDC"],
              ["personnage1"],
              ["objects"],
              "Le hall d'entrée de l'Ephec",
-             "Gildas attend le secrétariat")
+             "Gildas attend le secrétariat"),
 
-AIL01 = Area("Ancienne aile du rez de chaussée",
+"AIL01" : Area("Ancienne aile du rez de chaussée",
              "AIL01",
              ["Toilettes" , "Cafétéria" , "Escalier principal" , "HAL01", "Zone distributeur" , "Locaux RDC" , "Learning Lab"],
              ["personnage1"],
              ["objects"],
              "L'endroit ou on cours les enculés de market",
-             "Local on est 40 dedans")
+             "Local on est 40 dedans"),
 
-AIL02 = Area("Nouvelle aile du rez de chaussée",
+"AIL02" : Area("Nouvelle aile du rez de chaussée",
              "AIL01",
              ["LA01", "LA02", "Logette bois", "Escalier secondaire", "Secrétariat", "HAL01"],
              ["personnage1"],
              ["objects"],
              "Nouvelle aile",
-             "ça pue y PA")
+             "ça pue y PA"),
 
-LOC01 = Area("Les locaux du rez de chaussée",
+"LOC01" : Area("Les locaux du rez de chaussée",
              "LOC01",
              ["LR01", "LR02", "LR03", "LR04", "LR05", "AIL01"],
              ["personnage1"],
              ["objects"],
-             "Nouvelle aile",
-             "ça pue y PA")
+             "Les locaux de l'ancienne aile",
+             "ça pue y PA"),
+
+}
