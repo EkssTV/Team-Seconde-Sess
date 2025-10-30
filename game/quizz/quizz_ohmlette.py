58a27b7ba9ca68640e4164db7c6bbeb107168a9f
Erreur_504 <stefantroch@gmail.com>
2025-10-30 09:20:52 +0100

    aa

--- game/quizz/quizz_ohmlette.py
+++ game/quizz/quizz_ohmlette.py
@@ -1,25 +1,14 @@
 from quizz import Quizz
 #utilisation de la classe QUIZZ pour créer les différentes questions
-question1 = Quizz("Quelle est la bonne réponse?",
-                  ["la réponse 4","la réponse 4","la réponse 4","la réponse 4"],
+
+
+
+
+#Template
+question0 = Quizz("Question",
+                  ["1","2","3","4"],
                   3,
-                  "Ohmlette")
+                  "npc")
 
 
 
-
-
-
-
-
-
-
-
-#TEST en LOCAL (je vais supp)
-"""print(question1.ask())
-reponse = int(input("Donne ta réponse : "))
-
-if question1.verify_response(reponse):
-    print('bravo')
-else:
-    print('faux')"""
