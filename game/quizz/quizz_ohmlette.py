53251f9877639a4566f1c9814e648a1edd69556f
Erreur_504 <stefantroch@gmail.com>
2025-10-30 10:00:33 +0100

    implementation 3 questions simple prof Ohmlette + test des schémas

--- game/quizz/quizz_ohmlette.py
+++ game/quizz/quizz_ohmlette.py
@@ -2,13 +2,51 @@ from quizz import Quizz
 #utilisation de la classe QUIZZ pour créer les différentes questions
 
 
+schema1= """
+Voici le schéma n1 
++----------------------+  LEGENDE :
+|   (+) ──[ R ]──┐     |    (+) borne positive
+|                │     |    (-) borne négative
+|               (⏚)    |    [ R ] Résistance
+|                │     |    (⏚) terre
+|   (−)──────────┘     |
++----------------------+
+"""
+schema2 ="""
+Donne moi la résistance équivalente dans ce schéma :
 
++----------------------+  LEGENDE :
+|        3 Ohm            |
+|     ┌───[ R ]──┐     |    (+) borne positive
+|     |          |     |    [ R ] Résistance
+|     └───[ R ]──┘     |  
+|        6 Ohm         |
++----------------------+
+
+
+"""
 
 #Template
-question0 = Quizz("Question",
+"""question0 = Quizz("Question",
                   ["1","2","3","4"],
                   3,
                   "npc")
+"""
+#Question niveau 1
 
+question1 = Quizz("Donne moi la résistance nécéssaire pour faire fonctionne ce schéma :" + schema1,
+                  ["350Ohm","1kOhm","10A","4V"],
+                  2,
+                  "Ohmlette")
 
+question2= Quizz(" Defini moi La loi d'Ohm :",
+                 ["donne la relation entre la différence de potentiel aux bornes d'une résistance et le courant la traversant. La différence de potentiel est proportionnelle au courant.",
+                          "donne la relation entre la différence de potentiel aux bornes d'une résistance et le courant la traversant. La différence de potentiel est inversement proportionnelle au courant",
+                          "donne la relation entre la différence de potentiel aux bornes de n'importe quel dipôle électrique et le courant le traversant. La différence de potentiel est inversement proportionnelle au courant.",
+                          "donne la relation entre la différence de potentiel aux bornes de n'importe quel dipôle électrique et le courant le traversant. La différence de potentiel est proportionnelle au courant."],
+                1,
+                 "Ohmlette"
+                 )
 
+question3= Quizz(schema2,["3 Ohm","6 Ohm","9 Ohm","12 Ohm"],3,"Ohmlette")
+question3.ask()
