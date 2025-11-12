bac562fdb18342f7f37f168afe8e48ad0280e2bb
EkssTV <matthieu.decreme@gmail.com>
2025-11-12 20:24:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from game.player.player_class import Player
 from game.gui.gui_class import GameGUI,player
 from game.scripts.command_script_magnager import handle_command_from_gui
-from game.scripts.script_debut import debut_script
+
 
 gui = GameGUI(player)
 gui.starting_game()
