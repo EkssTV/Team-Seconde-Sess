084dba30836b6caeda6f36247938a6fe9ab5565d
Boureym0 <benjamin.junion@gmail.com>
2025-11-13 15:22:45 +0100

    adding object script

--- game/object/object_class.py
+++ game/object/object_class.py
@@ -9,16 +9,18 @@
 
 """
 #Class objet
-class ObjectInventory:
+class CreaObject:
     def __init__(self, id, nom, descri, utilite):
         self.__id = id
         self.__nom = nom
         self.descri = descri
         self.utilite = utilite
 
+    @property
     def id(self):
         return self.__id
 
+    @property
     def nom(self):
         return self.__nom
 
---
+++ game/object/object_scripting.py
@@ -0,0 +1,37 @@
+"""
+===============================================
+ EPHEC QUEST - object_class.py
+-----------------------------------------------
+ Description : scripting objects
+ Auteur      : Benjamin
+ Date        : 2025
+===============================================
+
+"""
+import csv
+import os
+from game.object.object_class import CreaObject
+
+def load_csv_object():
+    base_path = os.path.dirname(__file__) #dossier courant
+    file_path = os.path.join(base_path,"object_data.csv") #vient chercher le dossier
+
+    objects = {} #dictionnaire objects
+
+    with open(file_path,'r',encoding="utf-8") as csv_file:
+        reader = csv.reader(csv_file,delimiter=";")
+
+        next(reader) #saute la première ligne
+
+        #remplissage du dictionnaire
+        for row in reader:
+            obje = CreaObject(row[0],row[1],row[2],row[3]) #Création de l'objet
+            objects[row[0]] = obje #Remplissage du dictionnaire objects
+
+    return objects
+
+
+
+
+
+
