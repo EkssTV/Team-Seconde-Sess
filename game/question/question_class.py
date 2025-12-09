f730640af137833009812bd4ce78ba951d97ccd4
Erreur_504 <stefantroch@gmail.com>
2025-12-09 16:23:58 +0100

    Revision totaled de : question_class.py question_scripting.py creation de questions_data2.csv au bon format.
    Utilisation de CSV REader

--- game/question/question_class.py
+++ game/question/question_class.py
@@ -7,16 +7,25 @@
  Date        : 01/11/2025
 ===============================================
 """
+import re
 import random
 class Question:
     def __init__(self,idQuestion: int,question: str,answers : list, correct_answer: int):
         self.id = idQuestion
         self.question = question
-        self.answers = answers
-        self.correct_answer = int(correct_answer)
+        self.answers = [re.sub(r'^\s+|\s+$', '', a) for a in answers]
+        self._correct_answer = int(correct_answer)
 
-
-
+    #Getter
+    @property
+    def correct_answer(self):
+        return self._correct_answer
+    #Setter
+    @correct_answer.setter
+    def correct_answer(self,value):
+        if value == 0 or value > len(self.answers):
+            raise ValueError("Indice de la réponse invalide")
+        self._correct_answer = value
     def ask(self):
         bonne_reponse = self.answers[self.correct_answer]
         self.answers.sort(key=lambda x: random.random())
